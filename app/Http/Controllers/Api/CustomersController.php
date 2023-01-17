b7acbee4f558933baee27b055aa3f59ddc172cf4
maasiek <masiu.veb@gmail.com>
2023-01-17 18:56:59 +0100

    create api controller for customer, create routs for api

--- app/Http/Controllers/Api/CustomersController.php
+++ app/Http/Controllers/Api/CustomersController.php
@@ -3,9 +3,17 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Customer;
+use http\Client\Response;
 use Illuminate\Http\Request;
 
 class CustomersController extends Controller
 {
-    //
+    public function index()
+    {
+
+        $customers = Customer::all();
+
+    }
+
 }
